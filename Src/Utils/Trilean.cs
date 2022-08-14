15ebfdf33b3315b2f4de26706eb7b5c6084e63c4
-Unknown- <mrunknownyt@gmail.com>
2022-08-13 20:13:45 -0600

    Update Trilean.cs

--- Src/Utils/Trilean.cs
+++ Src/Utils/Trilean.cs
@@ -9,5 +9,7 @@ namespace USharpLibs.Common.Utils {
 		public static bool IsUnknown(this Trilean self) => self == Trilean.Unknown;
 		public static bool IsTrue(this Trilean self) => self == Trilean.True;
 		public static bool IsFalse(this Trilean self) => self == Trilean.False;
+
+		public static Trilean BoolToTrilean(bool b) => b ? Trilean.True : Trilean.False;
 	}
 }
