13c1b07e6b6fa7d08bb857494e153668bd3881e2
-Unknown- <mrunknownyt@gmail.com>
2022-08-21 22:27:25 -0600

    Update MathH.cs

--- Src/Utils/MathH.cs
+++ Src/Utils/MathH.cs
@@ -2,11 +2,17 @@ namespace USharpLibs.Common.Utils {
 	public static class MathH {
 		public static double ToRadians(double degrees) => degrees * Math.PI / 180d;
 		public static double ToDegrees(double rads) => rads * 180d / Math.PI;
+
 		public static int Floor(float value) => (int)MathF.Floor(value);
-		public static int Floor(double value) => (int)Math.Floor(value);
+		public static long Floor(double value) => (long)Math.Floor(value);
 		public static int Floor(decimal value) => (int)Math.Floor(value);
+
 		public static int Ceil(float value) => (int)MathF.Ceiling(value);
-		public static int Ceil(double value) => (int)Math.Ceiling(value);
+		public static long Ceil(double value) => (long)Math.Ceiling(value);
 		public static int Ceil(decimal value) => (int)Math.Ceiling(value);
+
+		public static int Round(float value) => (int)MathF.Round(value);
+		public static long Round(double value) => (long)Math.Round(value);
+		public static int Round(decimal value) => (int)Math.Round(value);
 	}
 }
