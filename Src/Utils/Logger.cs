46e8a911b1c3aebdb1016a70023cdd58f656ab35
-Unknown- <mrunknownyt@gmail.com>
2022-08-26 00:57:13 -0600

    Update Logger.cs

--- Src/Utils/Logger.cs
+++ Src/Utils/Logger.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace USharpLibs.Common.Utils {
 	public sealed class Logger {
@@ -39,16 +40,262 @@ namespace USharpLibs.Common.Utils {
 		public void ErrorLine(string message, [CallerMemberName] string method = "???", [CallerLineNumber] int line = int.MinValue) => Console.Error.WriteLine(Format(message, Red, method, line));
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
-		private string Format(string message, string color, string method, int line) {
-			string start = $"{color}[{DateTime.Now:HH:mm:ss:fff}]";
+		public void PrintException(Exception e) {
+			Exception inner = e;
+			while (true) {
+				if (inner.InnerException != null) {
+					inner = inner.InnerException;
+				} else { break; }
+			}
 
-			return level switch {
-				LogLevel.Minimal => $"{start} {message}",
-				LogLevel.Normal => $"{start} [{(!string.IsNullOrWhiteSpace(source) ? $"{source}/" : string.Empty)}{method}.{(line == int.MinValue ? "???" : line)}] {message}",
-				LogLevel.More => $"{start} [{(!string.IsNullOrWhiteSpace(source) ? $"{source}/" : string.Empty)}{new StackFrame(2, false).GetMethod()?.DeclaringType?.Name}.{method}.{(line == int.MinValue ? "???" : line)}] {message}",
-				LogLevel.Maximum => $"{start} [{(!string.IsNullOrWhiteSpace(source) ? $"{source}/" : string.Empty)}{new StackFrame(2, false).GetMethod()?.DeclaringType?.FullName}.{method}.{(line == int.MinValue ? "???" : line)}] {message}",
-				_ => throw new NotImplementedException(),
-			};
+			if (level == LogLevel.Minimal) {
+				Console.Error.WriteLine($"{GetStart(Red)} {inner.GetType().Name}: {inner.Message}");
+				return;
+			}
+
+			StackTrace trace = new(2, true);
+			StackFrame frame = trace.GetFrame(0) ?? throw new Exception();
+			string method = frame.GetMethod()?.Name ?? throw new Exception();
+
+			if (method == "<>c") { method = GetNamespace(trace.GetFrame(1) ?? throw new Exception()); }
+			if (method.Contains(".ctor")) { method = ".ctor"; }
+
+			if (level == LogLevel.Normal) {
+				Console.Error.WriteLine($"{GetStart(Red)} [{GetSource()}{method}.{GetLineNumber(frame.GetFileLineNumber())}] {inner.GetType().Name}: {inner.Message}");
+				return;
+			}
+
+			string @namespace = GetNamespace(frame);
+			if (@namespace == "<>c") { @namespace = GetNamespace(trace.GetFrame(1) ?? throw new Exception()); }
+
+			Console.Error.WriteLine($"{GetStart(Red)} [{GetSource()}{@namespace.Replace("+<>c", string.Empty)}.{method}.{GetLineNumber(frame.GetFileLineNumber())}] {inner.GetType().Name}: {inner.Message}");
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		private string Format(string message, string color, string method, int line) {
+			if (level == LogLevel.Minimal) {
+				return $"{GetStart(color)} {message}";
+			} else {
+				if (level == LogLevel.Normal) { return $"{GetStart(color)} [{GetSource()}{method}.{GetLineNumber(line)}] {message}"; }
+
+				StackTrace trace = new(2, false);
+				StackFrame frame = trace.GetFrame(0) ?? throw new Exception();
+
+				string @namespace = GetNamespace(frame);
+				if (@namespace == "<>c") { @namespace = GetNamespace(trace.GetFrame(1) ?? throw new Exception()); }
+
+				return $"{GetStart(color)} [{GetSource()}{@namespace.Replace("+<>c", string.Empty)}.{method}.{GetLineNumber(line)}] {message}";
+			}
+		}
+
+		private static string GetStart(string color) => $"{color}[{DateTime.Now:HH:mm:ss:fff}]";
+		private string GetNamespace(StackFrame frame) => level == LogLevel.More ? frame.GetMethod()?.DeclaringType?.Name ?? "???" : level == LogLevel.Maximum ? frame.GetMethod()?.DeclaringType?.FullName ?? "???" : throw new Exception($"Unknown logging level: {level}");
+		private string GetSource() => !string.IsNullOrWhiteSpace(source) ? $"{source}/" : string.Empty;
+		private static string GetLineNumber(int value) => value <= 0 ? "???" : value.ToString();
+	}
+
+	public class LoggerWriter : StreamWriter {
+		public override Encoding Encoding => Encoding.UTF8;
+		private readonly TextWriter old;
+
+		public LoggerWriter(TextWriter old, FileStream file) : base(file) {
+			this.old = old;
+			AutoFlush = true;
+		}
+
+		public override void Write(ReadOnlySpan<char> buffer) {
+			base.Write(buffer);
+			old.Write(buffer);
+		}
+
+		public override void Write(StringBuilder? value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(bool value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(char value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(char[] buffer, int index, int count) {
+			base.Write(buffer, index, count);
+			old.Write(buffer, index, count);
+		}
+
+		public override void Write(char[]? buffer) {
+			base.Write(buffer);
+			old.Write(buffer);
+		}
+
+		public override void Write(decimal value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(double value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(float value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(int value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(long value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(object? value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(string format, object? arg0) {
+			base.Write(format, arg0);
+			old.Write(format, arg0);
+		}
+
+		public override void Write(string format, object? arg0, object? arg1) {
+			base.Write(format, arg0, arg1);
+			old.Write(format, arg0, arg1);
+		}
+
+		public override void Write(string format, object? arg0, object? arg1, object? arg2) {
+			base.Write(format, arg0, arg1, arg2);
+			old.Write(format, arg0, arg1, arg2);
+		}
+
+		public override void Write(string format, params object?[] arg) {
+			base.Write(format, arg);
+			old.Write(format, arg);
+		}
+
+		public override void Write(string? value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(uint value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void Write(ulong value) {
+			base.Write(value);
+			old.Write(value);
+		}
+
+		public override void WriteLine() {
+			base.WriteLine();
+			old.WriteLine();
+		}
+
+		public override void WriteLine(ReadOnlySpan<char> buffer) {
+			base.WriteLine(buffer);
+			old.WriteLine(buffer);
+		}
+
+		public override void WriteLine(StringBuilder? value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(bool value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(char value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(char[] buffer, int index, int count) {
+			base.WriteLine(buffer, index, count);
+			old.WriteLine(buffer, index, count);
+		}
+
+		public override void WriteLine(char[]? buffer) {
+			base.WriteLine(buffer);
+			old.WriteLine(buffer);
+		}
+
+		public override void WriteLine(decimal value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(double value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(float value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(int value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(long value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(object? value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(string format, object? arg0) {
+			base.WriteLine(format, arg0);
+			old.WriteLine(format, arg0);
+		}
+
+		public override void WriteLine(string format, object? arg0, object? arg1) {
+			base.WriteLine(format, arg0, arg1);
+			old.WriteLine(format, arg0, arg1);
+		}
+
+		public override void WriteLine(string format, object? arg0, object? arg1, object? arg2) {
+			base.WriteLine(format, arg0, arg1, arg2);
+			old.WriteLine(format, arg0, arg1, arg2);
+		}
+
+		public override void WriteLine(string format, params object?[] arg) {
+			base.WriteLine(format, arg);
+			old.WriteLine(format, arg);
+		}
+
+		public override void WriteLine(string? value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(uint value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
+		}
+
+		public override void WriteLine(ulong value) {
+			base.WriteLine(value);
+			old.WriteLine(value);
 		}
 	}
 
