2b8f166973f0682700bd82fc6a42667a647ef312
-Unknown- <mrunknownyt@gmail.com>
2025-07-21 00:27:39 -0600

    C#14 extension support + removed old unused code

--- Src/Utils/Extensions.cs
+++ Src/Utils/Extensions.cs
@@ -3,44 +3,13 @@ using JetBrains.Annotations;
 
 namespace USharpLibs.Common.Utils {
 	[PublicAPI]
-	public static class CollectionExtension {
-		[MustUseReturnValue] public static string AsString<T>(this T[] self) => $"Size:{self.Length}, Elements:[{string.Join(", ", self)}]";
-		[MustUseReturnValue] public static string AsString<T>(this ICollection<T> self) => $"Size:{self.Count}, Elements:[{string.Join(", ", self)}]";
-	}
-
-	[PublicAPI]
-	public static class DictionaryExtension {
-		[MustUseReturnValue]
-		public static V ComputeIfAbsent<K, V>(this Dictionary<K, V> self, K key, Func<K, V> compute) where K : notnull {
-			if (self.TryGetValue(key, out V? absent)) { return absent; }
-
-			V value = compute(key);
-			self.Add(key, value);
-			return value;
+	public static class Extensions {
+		extension<T>(ICollection<T> self) {
+			[MustUseReturnValue] public string GetSizeAndElementsAsString() => $"Size:{self.Count}, Elements:[{string.Join(", ", self)}]";
 		}
 
-		[MustUseReturnValue] public static V? ComputeIfPresent<K, V>(this Dictionary<K, V> self, K key, Func<K, V, V> compute) where K : notnull => self.TryGetValue(key, out V? value) ? self[key] = compute(key, value) : default;
-
-		[MustUseReturnValue]
-		public static V? ComputeForBoth<K, V>(this Dictionary<K, V> self, K key, Func<K, V, V> ifPresent, Func<K, V> ifMissing) where K : notnull =>
-				self.ContainsKey(key) ? self.ComputeIfPresent(key, ifPresent) : self.ComputeIfAbsent(key, ifMissing);
-
-		[MustUseReturnValue]
-		public static V? TryGetValue<K, V>(this Dictionary<K, V> self, K key) where K : notnull {
-			self.TryGetValue(key, out V? value);
-			return value;
+		extension(Color self) {
+			[MustUseReturnValue] public string ToHex(bool includeHash = true) => $"{(includeHash ? '#' : string.Empty)}{self.R:X2}{self.G:X2}{self.B:X2}";
 		}
-	}
-
-	[PublicAPI]
-	public static class EnumerableExtension {
-		public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
-			foreach (T item in source) { action(item); }
-		}
-	}
-
-	[PublicAPI]
-	public static class ColorExtension {
-		[MustUseReturnValue] public static string ToHex(this Color self) => $"#{self.R:X2}{self.G:X2}{self.B:X2}";
 	}
 }
